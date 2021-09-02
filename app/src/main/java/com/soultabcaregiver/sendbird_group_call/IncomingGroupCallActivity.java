8f25f57cd23ee0c91b6901c1f9956d371f259c69
SumitTrivedi073 <33755744+SumitTrivedi073@users.noreply.github.com>
2021-09-02 16:23:16 +0530

    1) working on audio playing issue in incoming screen in group call functionality done

--- app/src/main/java/com/soultabcaregiver/sendbird_group_call/IncomingGroupCallActivity.java
+++ app/src/main/java/com/soultabcaregiver/sendbird_group_call/IncomingGroupCallActivity.java
@@ -1,7 +1,12 @@
 package com.soultabcaregiver.sendbird_group_call;
 
+import android.content.Context;
 import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.TextView;
@@ -13,6 +18,7 @@ import com.sendbird.calls.SendBirdCall;
 import com.soultabcaregiver.R;
 
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import static com.soultabcaregiver.sendbird_group_call.SendBirdGroupCallService.EXTRA_CHANNEL_URL;
@@ -20,84 +26,141 @@ import static com.soultabcaregiver.sendbird_group_call.SendBirdGroupCallService.
 import static com.soultabcaregiver.sendbird_group_call.SendBirdGroupCallService.EXTRA_ROOM_ID;
 
 public class IncomingGroupCallActivity extends AppCompatActivity {
-	
-	private static final String TAG = IncomingGroupCallActivity.class.getSimpleName();
-	
-	public static final String EXTRA_END_CALL = "end_call";
-	
-	private Room room;
-	
+
+    private static final String TAG = IncomingGroupCallActivity.class.getSimpleName();
+
+    public static final String EXTRA_END_CALL = "end_call";
+
+    private Room room;
+
+    AudioManager am;
+
+    private Vibrator mVibrator;
+
+    private boolean mVibrate = true;
+
+    MediaPlayer mediaPlayer;
+
+    private final long[] mVibratePattern = {0, 500, 500};
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
 	@Override
-	protected void onCreate(@Nullable Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		
-		if (getIntent().getBooleanExtra(EXTRA_END_CALL, false)) {
-			finish();
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getIntent().getBooleanExtra(EXTRA_END_CALL, false)) {
+            finish();
+        }
+
+        wakeScreen();
+
+        setContentView(R.layout.activity_incoming_group_call_screen);
+
+        TextView userName = findViewById(R.id.remoteUser);
+
+		am = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+
+
+		if (mVibrate) {
+			mVibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
 		}
-		
-		wakeScreen();
-		
-		setContentView(R.layout.activity_incoming_group_call_screen);
-		
-		TextView userName = findViewById(R.id.remoteUser);
-		
-		String roomId = getIntent().getStringExtra(EXTRA_ROOM_ID);
-		String channelUrl = getIntent().getStringExtra(EXTRA_CHANNEL_URL);
-		String userIds = getIntent().getStringExtra(EXTRA_GROUPS_USERS_IDS);
-		
-		GroupChannel.getChannel(channelUrl, (groupChannel, e) -> {
-			userName.setText(groupChannel.getName());
-		});
-		
-		SendBirdCall.fetchRoomById(roomId, (newRoom, e) -> {
-			room = newRoom;
-		});
-		
-		findViewById(R.id.answerButton).setOnClickListener(v -> {
-			EnterParams enterParams = new EnterParams();
-			enterParams.setAudioEnabled(true);
-			enterParams.setVideoEnabled(true);
-			if (room != null) {
-				
-				SendBirdGroupCallService.startService(IncomingGroupCallActivity.this, "GroupName",
-						room.getRoomId(), channelUrl, userIds, false);
-				
-				room.enter(enterParams, e1 -> {
-					Intent groupCallIntent = new Intent(this, GroupCallActivity.class);
-					groupCallIntent.putExtra(EXTRA_ROOM_ID, roomId);
-					groupCallIntent.putExtra(EXTRA_CHANNEL_URL, channelUrl);
-					groupCallIntent.putExtra(EXTRA_GROUPS_USERS_IDS, userIds);
-					startActivity(groupCallIntent);
-					finish();
-				});
+
+		mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.ringing);
+		mediaPlayer.setLooping(true);
+		mediaPlayer.start();
+
+
+		if (mVibrate) {
+			mVibrator.vibrate(mVibratePattern, 0);
+		}
+
+        String roomId = getIntent().getStringExtra(EXTRA_ROOM_ID);
+        String channelUrl = getIntent().getStringExtra(EXTRA_CHANNEL_URL);
+        String userIds = getIntent().getStringExtra(EXTRA_GROUPS_USERS_IDS);
+
+        GroupChannel.getChannel(channelUrl, (groupChannel, e) -> {
+            userName.setText(groupChannel.getName());
+        });
+
+        SendBirdCall.fetchRoomById(roomId, (newRoom, e) -> {
+            room = newRoom;
+        });
+
+        findViewById(R.id.answerButton).setOnClickListener(v -> {
+            EnterParams enterParams = new EnterParams();
+            enterParams.setAudioEnabled(true);
+            enterParams.setVideoEnabled(true);
+            if (room != null) {
+
+                SendBirdGroupCallService.startService(IncomingGroupCallActivity.this, "GroupName",
+                        room.getRoomId(), channelUrl, userIds, false);
+
+                room.enter(enterParams, e1 -> {
+                	if (mediaPlayer.isPlaying()) {
+						mediaPlayer.stop();
+						if (mVibrate)
+							mVibrator.cancel();
+					}
+                    Intent groupCallIntent = new Intent(this, GroupCallActivity.class);
+                    groupCallIntent.putExtra(EXTRA_ROOM_ID, roomId);
+                    groupCallIntent.putExtra(EXTRA_CHANNEL_URL, channelUrl);
+                    groupCallIntent.putExtra(EXTRA_GROUPS_USERS_IDS, userIds);
+                    startActivity(groupCallIntent);
+                    finish();
+                });
+            }
+        });
+
+        findViewById(R.id.declineButton).setOnClickListener(v -> {
+			if (mediaPlayer.isPlaying()) {
+				mediaPlayer.stop();
+				if (mVibrate)
+					mVibrator.cancel();
 			}
-		});
-		
-		findViewById(R.id.declineButton).setOnClickListener(v -> {
-			finish();
-		});
-	}
-	
+            finish();
+        });
+    }
+
 	@Override
-	protected void onNewIntent(Intent intent) {
-		super.onNewIntent(intent);
-		if (intent.getBooleanExtra(EXTRA_END_CALL, false)) {
-			if (room != null) {
-				if (room.getParticipants().size() <= 1) {
-					finish();
-				}
-			} else {
-				finish();
-			}
+	protected void onStop() {
+		super.onStop();
+		if (mediaPlayer.isPlaying()) {
+			mediaPlayer.stop();
+			if (mVibrate)
+				mVibrator.cancel();
 		}
 	}
-	
-	private void wakeScreen() {
-		Window window = getWindow();
-		window.addFlags(
-				WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON | WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD | WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		if (mediaPlayer.isPlaying()) {
+			mediaPlayer.stop();
+			if (mVibrate)
+				mVibrator.cancel();
+		}
 	}
-	
+
+	@Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        if (intent.getBooleanExtra(EXTRA_END_CALL, false)) {
+            if (room != null) {
+                if (room.getParticipants().size() <= 1) {
+                    finish();
+                }
+            } else {
+                finish();
+            }
+        }
+    }
+
+    private void wakeScreen() {
+        Window window = getWindow();
+        window.addFlags(
+                WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON | WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD | WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
+    }
+
 }
 
 
