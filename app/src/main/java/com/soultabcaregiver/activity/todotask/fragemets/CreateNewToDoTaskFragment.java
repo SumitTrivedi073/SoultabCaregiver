683fc228385ab274c3944d0a68c7034d3d6b23d5
sumit <sumittrivedi073@gmail.com>
2021-12-15 18:38:37 +0530

    1)add formate to pick word file 2) app crash issue fix in companion login

--- app/src/main/java/com/soultabcaregiver/activity/todotask/fragemets/CreateNewToDoTaskFragment.java
+++ app/src/main/java/com/soultabcaregiver/activity/todotask/fragemets/CreateNewToDoTaskFragment.java
@@ -652,8 +652,8 @@ public class CreateNewToDoTaskFragment extends Fragment {
 		intent.setAction(Intent.ACTION_GET_CONTENT);
 		intent.setType("*/*");
 		intent.putExtra(Intent.EXTRA_MIME_TYPES,
-				new String[]{"application/pdf",
-						".openxmlformats-officedocument.wordprocessingml.document",
+				new String[]{"application/pdf","application/msword",
+						"application/vnd.openxmlformats-officedocument.wordprocessingml.document",
 						"image/jpeg", "image/png", "image/gif"});
 		startActivityForResult(Intent.createChooser(intent, "Select File"), PICK_DOCUMENTS);
 	}
